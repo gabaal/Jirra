e170a7119ecec5f08ae50187ff002afb43befe2a
Gerald <geraldbaalham@live.co.uk>
2025-03-05 12:27:15 +0000

    4:07 sidebar, mobile sidebar, navbar complete

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,8 @@
+import { getCurrent } from "@/features/auth/actions";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+  return <div>Main Page</div>;
+}

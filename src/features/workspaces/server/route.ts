d56e7fff3030494f092f103a08315a1135bc9ac4
Gerald <geraldbaalham@live.co.uk>
2025-03-06 10:19:22 +0000

    5:38 Members and invite code complete

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -9,8 +9,9 @@ import {
   MEMBERS_ID,
   WORKSPACES_ID,
 } from "@/config";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { MemberRole } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
 
@@ -18,11 +19,25 @@ const app = new Hono()
     "/", // => /workspaces
     sessionMiddleware,
     async (c) => {
+      const user = c.get("user");
       const databases = c.get("databases");
+
+      const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+        Query.equal("userId", user.$id),
+      ]);
+
+      if (!members.documents.length) {
+        return c.json({ data: { documents: [], total: 0 } });
+      }
+
+      const workspaceIds = members.documents.map(
+        (member) => member.workspaceId
+      );
 
       const workspaces = await databases.listDocuments(
         DATABASE_ID,
-        WORKSPACES_ID
+        WORKSPACES_ID,
+        [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)]
       );
       return c.json({ data: workspaces });
     }
@@ -66,6 +81,7 @@ const app = new Hono()
           name,
           userId: user.$id,
           imageUrl: uploadedImageUrl,
+          inviteCode: generateInviteCode(6),
         }
       );
 

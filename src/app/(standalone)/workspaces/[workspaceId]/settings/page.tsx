972efe0ade694b3121450399afa8384ae506ea0d
Gerald <geraldbaalham@live.co.uk>
2025-03-07 11:41:00 +0000

    6:51 workspace form showing

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,4 +1,29 @@
-const WorkspaceIdSettingsPage = () => {
-  return <div>WorkspaceIdSettingsPage</div>;
+import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
+import { redirect } from "next/navigation";
+
+interface WorkspaceIdSettingsPageProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const WorkspaceIdSettingsPage = async ({
+  params,
+}: WorkspaceIdSettingsPageProps) => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+  if (!initialValues) {
+    redirect(`/workspaces/${params.workspaceId}`);
+  }
+  return (
+    <div>
+      Workspace Id Settings Page {params.workspaceId}
+      <EditWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
 };
 export default WorkspaceIdSettingsPage;

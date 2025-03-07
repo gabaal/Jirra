5647e2c5aa6100863cbcc828da6e706a3d617c39
Gerald <geraldbaalham@live.co.uk>
2025-03-07 18:18:36 +0000

    7:08 REFACTOR 1 DONE

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -3,6 +3,29 @@ import "server-only";
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Client, Account, Storage, Users, Databases } from "node-appwrite";
 import { cookies } from "next/headers";
+import { AUTH_COOKIE } from "@/features/auth/constants";
+
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+  const session = await cookies().get(AUTH_COOKIE);
+
+  if (!session || !session.value) {
+    throw new Error("Unauthorised");
+  }
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+}
 
 export async function createAdminClient() {
   const client = new Client()
